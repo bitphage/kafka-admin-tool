0e595a4bb3ac81e2b48e685bb7b2e972eb7f2e58
Vladimir Kamarzin <vvk@vvk.pp.ru>
2020-12-16 18:51:37 +0500

    Move logger configurator to functions

--- kafkaadmin/admin.py
+++ kafkaadmin/admin.py
@@ -4,27 +4,18 @@ from typing import List, Optional, Set
 import yaml
 from kafka import KafkaAdminClient, KafkaConsumer
 
+from kafkaadmin.functions import set_debug
+
 log = logging.getLogger(__name__)
 
 
 class Admin:
     def __init__(self, config: str, debug: bool):
-        self.set_debug(debug)
+        set_debug(log, debug)
         self.config = yaml.safe_load(config)
 
         self.admin = KafkaAdminClient(bootstrap_servers=self.config["bootstrap_servers"])
         self.consumer = KafkaConsumer(bootstrap_servers=self.config["bootstrap_servers"])
-
-    @staticmethod
-    def set_debug(debug: bool) -> None:
-        if debug is True:
-            log.setLevel(logging.DEBUG)
-        else:
-            log.setLevel(logging.INFO)
-        handler = logging.StreamHandler()
-        formatter = logging.Formatter("%(asctime)s %(levelname)s: %(message)s")
-        handler.setFormatter(formatter)
-        log.addHandler(handler)
 
     def get_topics_by_prefix(self, prefix: Optional[str] = None) -> List[str]:
         """Return all topics matched prefix."""
---
+++ kafkaadmin/functions.py
@@ -0,0 +1,12 @@
+import logging
+
+
+def set_debug(log: logging.Logger, debug: bool) -> None:
+    if debug is True:
+        log.setLevel(logging.DEBUG)
+    else:
+        log.setLevel(logging.INFO)
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter("%(asctime)s %(levelname)s: %(message)s")
+    handler.setFormatter(formatter)
+    log.addHandler(handler)
